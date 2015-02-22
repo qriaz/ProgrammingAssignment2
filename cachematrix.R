893949b5c9ed55b0dce134ccb4d8174489f98df7
qriaz <qammer.riaz@gmail.com>
2015-02-22 21:54:52 +0000

    Use makeVector and cacheMean as template
    Replace mean with solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly 
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(
+       set = set, 
+       get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse
+       )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: calculates the inverse of the special "matrix" created with above function 
+## IF inverse of the matrix has already been calculated. 
+## THEN get the inverse from the cache and do not compute. 
+## ELSE calculate inverse of matrix and set in cache using setinverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
